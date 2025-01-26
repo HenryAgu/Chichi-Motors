633a62e32b208211eda866b3f537fc68cda52436
Henry Agu <aguhenrychuks@gmail.com>
2025-01-26 14:47:33 +0100

    fix: footer for mobile

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -52,29 +52,29 @@ const Footer = () => {
               src="/images/location.svg"
               width={50}
               height={50}
-              className="h-[50px] w-[50px]"
+              className="md:h-[50px] md:w-[50px] h-[40px] w-[40px] mb-2"
               alt="icon"
             />
             <p className="text-[32px] md:text-[42px] text-white font-bold mb-2">Find our Store</p>
-            <span className="text-base text-white font-normal tracking-tight">
+            <span className="text-sm md:text-base text-white font-normal tracking-tight">
               Plot 165 G.U. Ake Road <br /> Beside Owo Filling Station, Eligbolo
               <br /> Port Harcourt, Rivers State
             </span>
           </div>
-          <div className="pt-14 border-b md:border-b-0 border-brand-green-200 pb-5 md:pb-0">
+          <div className="pt-10 md:pt-14">
             <p className="text-[32px] md:text-[42px] text-white font-bold mb-2">Contact Us</p>
-            <span className="text-base text-white font-normal tracking-tight">
+            <span className="text-sm md:text-base text-white font-normal tracking-tight">
               E-mail: chichimotors@gmail.com <br />
               Tel: 09029465338
             </span>
           </div>
         </div>
-        <div className="basis-[60%] pt-16 md:pl-24 pb-[32px] flex flex-col gap-y-12 md:gap-y-[74px]">
-          <div className="flex flex-col gap-y-4">
+        <div className="basis-[60%] pt-16 md:pl-24 pb-[32px] flex flex-col gap-y-8 md:gap-y-[80px]">
+          <div className="flex flex-col gap-y-2 md:gap-y-4 border-t border-brand-green-200 pt-8 md:pt-0 md:border-t-0">
             {navigation.map((item) => (
               <Link
                 href={item.path}
-                className="font-semibold text-white text-lg tracking-tighter capitalize transition-all duration-200 ease-out hover:underline"
+                className="font-semibold text-white text-base md:text-lg tracking-tighter capitalize transition-all duration-200 ease-out hover:underline"
                 key={item.title}
               >
                 {item.title}
@@ -82,21 +82,21 @@ const Footer = () => {
             ))}
           </div>
           <div className="flex flex-col">
-            <Link href="/contact" className="flex items-center gap-x-2">
+            <Link href="/contact" className="flex items-center gap-x-1 md:gap-x-2">
               <span className="text-[32px] md:text-[42px] font-bold text-white underline">
                 Get a Quote Today
               </span>
-              <Image src="/images/arrow.svg" alt="icon" width={27} height={27} className="h-[27px] w-[27px]"/>
+              <Image src="/images/arrow.svg" alt="icon" width={27} height={27} className="md:h-[27px] md:w-[27px] h-6 w-6"/>
             </Link>
-            <span className="text-base text-white font-normal tracking-tight">
+            <span className="text-sm md:text-base text-white font-normal tracking-tight">
               Leave us a message on WhatsApp
             </span>
           </div>
-          <div className="flex flex-col gap-y-3">
-            <p className="font-normal text-[32px] tracking-[3%] text-white">
+          <div className="flex flex-col gap-y-3 mt-5 md:mt-0">
+            <p className="font-normal text-2xl md:text-[32px] tracking-tighter text-white">
               Follow our Socials
             </p>
-            <div className="flex items-center gap-x-6">
+            <div className="flex items-center gap-x-3 md:gap-x-6">
               {socials.map((item) => (
                 <Link href={item.path} key={item.id}>
                   <Image
@@ -104,7 +104,7 @@ const Footer = () => {
                     alt="icon"
                     width={32}
                     height={32}
-                    className="aspect-square h-[32px] w-[32px]"
+                    className="aspect-square md:h-[32px] md:w-[32px] h-6 w-6"
                   />
                 </Link>
               ))}
