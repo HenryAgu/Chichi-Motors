134336ea7e8baddc6e8a112a1c10646ddfa5522f
Henry Agu <aguhenrychuks@gmail.com>
2025-01-27 11:24:42 +0100

    feat: landpage

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -6,27 +6,28 @@ interface Testimonials {
 }
 
 const testimonial: Testimonials[] = [
-  {
-    content:
-      "At Chi Chi Motors, there are no hidden fees or surprises. The price you see is the price you’ll pay. We believe in clear, upfront pricing so you can make.",
-    author: "Henry Agu Chukwuma",
-  },
-  {
-    content:
-      "At Chi Chi Motors, there are no hidden fees or surprises. The price you see is the price you’ll pay. We believe in clear, upfront pricing so you can make.",
-    author: "Henry Agu Chukwuma",
-  },
-  {
-    content:
-      "At Chi Chi Motors, there are no hidden fees or surprises. The price you see is the price you’ll pay. We believe in clear, upfront pricing so you can make.",
-    author: "Henry Agu Chukwuma",
-  },
-  {
-    content:
-      "At Chi Chi Motors, there are no hidden fees or surprises. The price you see is the price you’ll pay. We believe in clear, upfront pricing so you can make.",
-    author: "Henry Agu Chukwuma",
-  },
-];
+    {
+      content:
+        "At Chi Chi Motors, there are no hidden fees or surprises. The price you see is the price you&apos;ll pay. We believe in clear, upfront pricing so you can make.",
+      author: "Henry Agu Chukwuma",
+    },
+    {
+      content:
+        "At Chi Chi Motors, there are no hidden fees or surprises. The price you see is the price you&apos;ll pay. We believe in clear, upfront pricing so you can make.",
+      author: "Kalu Daniel Obinna",
+    },
+    {
+      content:
+        "At Chi Chi Motors, there are no hidden fees or surprises. The price you see is the price you&apos;ll pay. We believe in clear, upfront pricing so you can make.",
+      author: "Joel Okpara",
+    },
+    {
+      content:
+        "At Chi Chi Motors, there are no hidden fees or surprises. The price you see is the price you&apos;ll pay. We believe in clear, upfront pricing so you can make.",
+      author: "Leonard Ijeoma",
+    },
+  ];
+  
 
 const Testimonial = () => {
   return (
@@ -40,22 +41,23 @@ const Testimonial = () => {
           When you buy from us, you’re becoming part of our family.
         </p>
       </div>
-      <div className="overflow-x-auto testimony-container w-full flex gap-x-6 mt-10" >
+      <div className="overflow-x-auto testimony-container w-full flex gap-x-6 mt-10">
         {testimonial.map((item) => (
-          <div className="border border-[#E1E1E1] flex flex-col shrink-0 px-5 py-10 rounded-lg">
+          <div
+            className="border border-[#E1E1E1] flex flex-col shrink-0 px-5 py-10 rounded-lg"
+            key={item.author}
+          >
             <span className="text-5xl text-brand-green-100 font-medium pb-[-50px]">
               "
             </span>
             <p className="max-w-[321px] text-black text-base font-normal">
-              At Chi Chi Motors, there are no hidden fees or surprises. The
-              price you see is the price you’ll pay. We believe in clear,
-              upfront pricing so you can make.
+              {item.content}
             </p>
             <span className="text-5xl text-brand-green-100 font-medium flex justify-end">
               "
             </span>
             <span className=" text-[#969696] font-semibold text-[13px] mt-[-30px]">
-              Henry Agu Chukwuma
+              {item.author}
             </span>
           </div>
         ))}
