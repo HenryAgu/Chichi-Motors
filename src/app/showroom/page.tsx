666c009e6783b59f24416eb4970bb624bbb3961f
Kalu Daniel <krayzhee419@gmail.com>
2025-01-31 10:36:21 +0100

    feat:  show message when no cars are available

--- src/app/showroom/page.tsx
+++ src/app/showroom/page.tsx
@@ -177,54 +177,62 @@ export function CarShow({
 	const displayedCars = limit ? filteredCars?.slice(0, limit) : filteredCars;
 
 	return (
-		<section className="mt-10 grid grid-cols-1 md:grid-cols-3 gap-6 pb-28 pt-5 overflow-hidden">
-			{displayedCars?.map((car) => (
-				<div key={car?.slug?.current} className="border p-4 rounded-[24px]">
-					<Image
-						src={car?.image?.asset?.url}
-						alt={car?.name}
-						width={397}
-						height={322}
-						className="object-cover mb-4 rounded-[8px] w-full h-[322px]"
-					/>
-					<div className="flex flex-col gap-y-3">
-						<div className="flex flex-col gap-y-3">
-							<p className="text-[13px] text-brand-green-100 font-semibold leading-[17.7px]">
-								{car?.type}
-							</p>
-							<h2 className="text-lg font-bold leading-[24.51px] text-black">
-								{car?.year} {car?.brand} {car?.name}
-							</h2>
-						</div>
-						<div className="flex gap-x-4">
-							<p className="inline-flex items-center gap-x-2 text-[13px] font-semibold leading-[17.7px] text-[#969696]">
-								<span
-									className="h-[16px] w-[16px] rounded-full"
-									style={{ backgroundColor: car?.exteriorColor }}
-								/>
-								{car?.exteriorColor} Exterior
-							</p>
-							<p className="inline-flex items-center gap-x-2 text-[13px] font-semibold leading-[17.7px] text-[#969696]">
-								<span
-									className="h-[16px] w-[16px] rounded-full"
-									style={{ backgroundColor: car?.interiorColor }}
-								/>
-								{car?.interiorColor} Interior
-							</p>
-						</div>
-
-						<p className="font-bold leading-[32.68px] text-2xl text-brand-green-100">
-							{car?.price}
-						</p>
-						<Link
-							href={`/showroom/${car?.slug?.current}`}
-							className="px-8 py-3 w-full flex items-center justify-center bg-brand-green-100 rounded-[32px] leading-[32.68px] md:text-xl text-lg font-bold mt-5 text-white"
-						>
-							Show Details
-						</Link>
-					</div>
+		<>
+			{displayedCars?.length === 0 ? (
+				<div className="flex justify-center items-center h-screen">
+					<p className="text-xl font-bold text-brand-green-100">Car not available</p>
 				</div>
-			))}
-		</section>
+			) : (
+				<section className="mt-10 grid grid-cols-1 md:grid-cols-3 gap-6 pb-28 pt-5 overflow-hidden">
+					{displayedCars?.map((car) => (
+						<div key={car?.slug?.current} className="border p-4 rounded-[24px]">
+							<Image
+								src={car?.image?.asset?.url}
+								alt={car?.name}
+								width={397}
+								height={322}
+								className="object-cover mb-4 rounded-[8px] w-full h-[322px]"
+							/>
+							<div className="flex flex-col gap-y-3">
+								<div className="flex flex-col gap-y-3">
+									<p className="text-[13px] text-brand-green-100 font-semibold leading-[17.7px]">
+										{car?.type}
+									</p>
+									<h2 className="text-lg font-bold leading-[24.51px] text-black">
+										{car?.year} {car?.brand} {car?.name}
+									</h2>
+								</div>
+								<div className="flex gap-x-4">
+									<p className="inline-flex items-center gap-x-2 text-[13px] font-semibold leading-[17.7px] text-[#969696]">
+										<span
+											className="h-[16px] w-[16px] rounded-full"
+											style={{ backgroundColor: car?.exteriorColor }}
+										/>
+										{car?.exteriorColor} Exterior
+									</p>
+									<p className="inline-flex items-center gap-x-2 text-[13px] font-semibold leading-[17.7px] text-[#969696]">
+										<span
+											className="h-[16px] w-[16px] rounded-full"
+											style={{ backgroundColor: car?.interiorColor }}
+										/>
+										{car?.interiorColor} Interior
+									</p>
+								</div>
+
+								<p className="font-bold leading-[32.68px] text-2xl text-brand-green-100">
+									{car?.price}
+								</p>
+								<Link
+									href={`/showroom/${car?.slug?.current}`}
+									className="px-8 py-3 w-full flex items-center justify-center bg-brand-green-100 rounded-[32px] leading-[32.68px] md:text-xl text-lg font-bold mt-5 text-white"
+								>
+									Show Details
+								</Link>
+							</div>
+						</div>
+					))}
+				</section>
+			)}
+		</>
 	);
 };
